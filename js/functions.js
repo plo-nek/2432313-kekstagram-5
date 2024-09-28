d9530bcf65a1de13505ee2c473fadbaec171dc61
plo-nek <nick130604@gmail.com>
2024-09-29 03:42:01 +0500

    new functions

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,33 @@
 const getCompareString = (str, maxLength)=>{
-  if(String.length(str) <= maxLength){
+  if(str.length <= maxLength){
     return true;
   }
   return false;
 };
-getCompareString('проверяемая строка', 20);
+console.log(getCompareString('проверяемая строка', 18));
+
+const getCheckString = (str)=>{
+  let newStr = str.replaceAll(' ','');
+  newStr = newStr.toUpperCase();
+  let space = '';
+  for (let i = newStr.length - 1; i >= 0; i --){
+    space += newStr[i];
+  }
+  return space === newStr;
+};
+console.log(getCheckString('Лёша на полке клопа нашёл '));
+
+const getNumberOfString = (str) =>{
+  let space = '';
+  for (let i = 0; i <= str.length; i ++){
+    let newChar = str[i];
+    if (isNaN(parseInt(newChar,10)) === false){
+      space += str[i];
+    }
+  }
+  parseInt(space,10);
+  isNaN(space);
+  return space.toString();
+};
+console.log(getNumberOfString('а я томат'));
+
